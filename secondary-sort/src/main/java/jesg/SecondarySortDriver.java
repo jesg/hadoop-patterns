df8ffe86f900c23c17e1b36af819843ac5aa2771
jesg <savagejas@gmail.com>
2014-01-10 07:42:48 -0800

    clean imports

--- secondary-sort/src/main/java/jesg/SecondarySortDriver.java
+++ secondary-sort/src/main/java/jesg/SecondarySortDriver.java
@@ -10,9 +10,7 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
